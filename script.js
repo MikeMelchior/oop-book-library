e64292ec01f255d3f0d99f62dd2da24e350c9f73
Mike Melchior <mike.melchior@outlook.com>
2022-06-01 16:22:17 -0400

    add delete book function

--- script.js
+++ script.js
@@ -24,10 +24,19 @@ function addBookToLibrary(book) {
 /* add in a few books manually to vizualize */
 const toKillAMockingbird = new Book('To Kill a Mockingbird', 'Harper Lee', 281, true);
 const theGreatGatsby = new Book('The Great Gatsby', 'F. Scott Fitzgerald', 208, false);
+const aCatcherInTheRye = new Book('A Catcher in the Rye', 'J.D. Salinger', 234, false)
 addBookToLibrary(toKillAMockingbird);
 addBookToLibrary(theGreatGatsby);
+addBookToLibrary(aCatcherInTheRye);
 
 
+
+
+
+
+function testFunc(e) {
+    console.log(e)
+}
 function makeHeader() {
     const header = document.createElement('div');
     header.classList.add('header');
@@ -35,11 +44,17 @@ function makeHeader() {
 }
 makeHeader();
 
-const div = document.createElement('div');
-div.classList.add('main');
-root.appendChild(div);
+function makeMain() {
+    const div = document.createElement('div');
+    div.classList.add('main');
+    root.appendChild(div);
+}
+makeMain();
 
-
+function removeMain() {
+    let main = document.querySelector('.main');
+    root.removeChild(main);
+}
 
 function addHeaderContent() {
     const button = document.createElement('button');
@@ -58,9 +73,11 @@ function addHeaderContent() {
 }
 addHeaderContent();
 
+
+
 function displayBooks() {
     myLibrary.forEach(book => {
-        let bookNumber = myLibrary.indexOf(book) + 1;
+        let bookNumber = myLibrary.indexOf(book);
         const div = document.createElement('div');
         div.classList.add(`book-${bookNumber}`);
 
@@ -77,11 +94,30 @@ function displayBooks() {
         para.textContent = book.haveRead;
         div.appendChild(para);
 
+        let button = document.createElement('button');
+        button.classList.add(`button-${bookNumber}`)
+        button.textContent = 'Remove Book'
+        div.appendChild(button);
+
         const main = document.querySelector('.main')
         main.appendChild(div);   
+
+        let deleteButton = document.querySelector(`.button-${bookNumber}`);
+        deleteButton.addEventListener('click', deleteBook)
     })
 };
+
  displayBooks();
 
+ function deleteBook(e) {
+    console.log(e.target.classList[0].split('-')[1])
+    console.log(e.target.offsetParent);
+    myLibrary.splice(e.target.classList[0].split('-')[1], 1);
+    removeMain();
+    makeMain();
+    displayBooks();
+}
 
+
+document.querySelector('.add-book').addEventListener('click', testFunc)
 
