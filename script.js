f65ebfc89d90701859aa181686f75c7abc9cf040
Mike Melchior <mike.melchior@outlook.com>
2022-06-01 14:27:36 -0400

    finish display books function

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 
 let myLibrary = [];
-let bookNumber = 0; 
 
 const root = document.querySelector('.root');
 
@@ -9,8 +8,12 @@ const root = document.querySelector('.root');
 function Book(title, author, pages, haveRead) {
     this.title = title;
     this.author = author;
-    this.pages = pages;
-    this.haveRead = haveRead;
+    this.pages = `${pages} pages`;
+    if (haveRead) {
+        this.haveRead = 'Have read'
+    } else {
+        this.haveRead = 'Have not read'
+    }
 }
 
 function addBookToLibrary(book) {
@@ -18,7 +21,11 @@ function addBookToLibrary(book) {
 }
 
 
-
+/* add in a few books manually to vizualize */
+const toKillAMockingbird = new Book('To Kill a Mockingbird', 'Harper Lee', 281, true);
+const theGreatGatsby = new Book('The Great Gatsby', 'F. Scott Fitzgerald', 208, false);
+addBookToLibrary(toKillAMockingbird);
+addBookToLibrary(theGreatGatsby);
 
 
 function makeHeader() {
@@ -51,16 +58,30 @@ function addHeaderContent() {
 }
 addHeaderContent();
 
-function displayBook() {
+function displayBooks() {
     myLibrary.forEach(book => {
-        bookNumber += 1;
+        let bookNumber = myLibrary.indexOf(book) + 1;
         const div = document.createElement('div');
-        div.classList.add(`card-${bookNumber}`);
-        const main = document.querySelector('.main')
-        main.appendChild(div);
-    })
-}
+        div.classList.add(`book-${bookNumber}`);
 
+        let para = document.createElement('p');
+        para.textContent = book.title;
+        div.appendChild(para);
+        para = document.createElement('p');
+        para.textContent = book.author;
+        div.appendChild(para);
+        para = document.createElement('p');
+        para.textContent = book.pages;
+        div.appendChild(para);
+        para = document.createElement('p');
+        para.textContent = book.haveRead;
+        div.appendChild(para);
+
+        const main = document.querySelector('.main')
+        main.appendChild(div);   
+    })
+};
+ displayBooks();
 
 
 
